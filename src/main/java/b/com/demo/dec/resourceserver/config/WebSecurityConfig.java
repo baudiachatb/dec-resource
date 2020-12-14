965946043e04634ca9315727ef8b4a7f0f689134
Bau Nguyen Van <baudcctak54@gmail.com>
2020-12-14 21:01:50 +0700

    first

---
+++ src/main/java/b/com/demo/dec/resourceserver/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package b.com.demo.dec.resourceserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .permitAll()
+                .and()
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
---
+++ src/test/java/b/com/demo/dec/resourceserver/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package b.com.demo.dec.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
